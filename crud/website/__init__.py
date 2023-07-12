be29258a2cc2aff0f9f342d214e8a5223eb50bab
McKinley <caiohudson.red4@hotmail.com>
2023-07-12 01:42:06 -0300

    Relatorio + Banco de Dados sendo feitos

--- crud/website/__init__.py
+++ crud/website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -21,10 +23,15 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    with app.app_context():
+        db.create_all()
+        
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     return app
 
-def create_database(app):
-    if not path('website/' + DB_NAME):
-        db.create_all(app=app)
-        print("Banco de Dados Criado!")
