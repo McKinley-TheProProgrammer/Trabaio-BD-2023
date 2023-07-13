acda615ef05e94167b80e44f9652ad6fdd7f4e14
McKinley <caiohudson.red4@hotmail.com>
2023-07-13 03:40:00 -0300

    Site saindo

--- crud/website/auth.py
+++ crud/website/auth.py
@@ -6,9 +6,9 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth',__name__)
 
-@auth.route("/login")
+@auth.route("/login",methods=['GET','POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html",boolean=True)
 
 @auth.route('/logout')
 def logout():
@@ -20,28 +20,32 @@ def sign_up():
 
     if request.method == 'POST':
             mat = request.form.get('mat')
-            first_name = request.form.get('nome')
             email = request.form.get('email')
+            nome = request.form.get('nome')
+            curso = request.form.get('curso')
             password1 = request.form.get('password1')
             password2 = request.form.get('password2')
 
-            user = User.query.filter_by(email=email).first()
-            if user:
-                flash('Email already exists.', category='error')
-            elif len(email) < 4:
-                flash('Email must be greater than 3 characters.', category='error')
-            elif len(first_name) < 2:
-                flash('First name must be greater than 1 character.', category='error')
+            #user = User.query.filter_by(email=email).first()
+            #if user:
+            #    flash('Email já existe.', category='error')
+            if len(email) < 4:
+                flash('Email precisa ter no minimo 4 caracteres.', category='error')
+            elif len(nome) < 2:
+                flash('Nome precisa ter no minimo 2 caracteres', category='error')
+            elif len(curso) < 8:
+                flash('Curso precisa ter no minimo 8 caracteres', category='error')
             elif password1 != password2:
-                flash('Passwords don\'t match.', category='error')
+                flash('As senhas não são iguais', category='error')
             elif len(password1) < 7:
-                flash('Password must be at least 7 characters.', category='error')
+                flash('A senha precisa ter no minimo 8 caracteres', category='error')
             else:
-                new_user = User(mat=mat, first_name=first_name,email=email, password=generate_password_hash(
+                new_user = User(email=email,nome=nome,curso=curso, password=generate_password_hash(
                     password1, method='sha256'))
                 db.session.add(new_user)
                 db.session.commit()
                 login_user(new_user, remember=True)
-                flash('Account created!', category='success')
+                flash('Conta Registrada!', category='success')
                 return redirect(url_for('views.home'))
+            
     return render_template("sign_up.html",user=current_user)
--- crud/website/models.py
+++ crud/website/models.py
@@ -3,20 +3,20 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 class Note(db.Model):
-    mat = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(150))
     data = db.Column(db.String(20000)) # 20000 caracteres
     date = db.Column(db.DateTime(timezone=True),default=func.now())
-    user_mat = db.Column(db.Integer,db.ForeignKey("user.mat")) # FK 
-    user_name = db.Column(db.String(150),db.ForeignKey("user.nome"))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id')) # FK 
+    #user_name = db.Column(db.String(150),db.ForeignKey("user.nome"))
 
 class User(db.Model, UserMixin):
-    mat = db.Column(db.Integer,primary_key=True) # INT PK
-    nome = db.Column(db.String(150)) # VARCHAR(150) UN
-    curso = db.Column(db.String(6))
+    id = db.Column(db.Integer,primary_key=True) # INT PK
     email = db.Column(db.String(150),unique=True) #VARCHAR(150) UN
+    nome = db.Column(db.String(150)) # VARCHAR(150) UN
+    curso = db.Column(db.String(150))
     password = db.Column(db.String(150)) #VARCHAR(150) UN
-    notes = db.relationship("Note") # INSERT INTO user
+    notes = db.relationship('Note') # INSERT INTO user
     
 
 class Course(db.Model):
